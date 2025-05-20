c8cde1aff799935d61c3ed01cb8312d26875506d
RomanOze <rumuislam202@gmail.com>
2025-05-20 21:53:18 +0600

    update

---
+++ script.js
@@ -0,0 +1,55 @@
+
+// var num = 27.35435 ;
+
+
+// console.log(num.toPrecision(3));
+
+// console.log(num.toFixed(2));
+
+
+
+var name =  'Roman Islam Rumu' ;
+ 
+// var check = name.charAt(5);
+var len = name.length;
+var tokra = name.slice(6 ,16);
+var upper = tokra.toUpperCase();
+var lower = tokra.toLowerCase();
+var sliceLength = tokra.length;
+
+console.log("Length means total Size : "+len);
+console.log("Display slice to  :"+tokra);
+console.log("Display slice to  :"+sliceLength);
+console.log("upper letter : "+upper);
+console.log("lower letter : "+lower);
+console.log('check the character  :'+name.charAt(1));
+
+
+
+var text  = prompt("Inter Your Number : ")
+
+console.log("Save Name : " + name.concat(text));
+
+
+
+
+
+
+
+
+
+
+
+// const age = 18 ;
+
+// if( age>18){
+//     alert('You are an adult');
+// }
+// else{
+//     alert('You are a child You can not access');
+// }
+
+
+// alert('ROMAN')
+// console.log('Islam')
+
